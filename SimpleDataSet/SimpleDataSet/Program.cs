00e10bc014591eeecf556341c49b5627bdf4f241
unknown <jakub.zezula@hotmail.com>
2023-07-20 23:53:23 +0200

    Continuation of excercise

--- SimpleDataSet/SimpleDataSet/Program.cs
+++ SimpleDataSet/SimpleDataSet/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using static System.Console;
+using System.Collections;
 
 namespace SimpleDataSet
 {
@@ -33,10 +34,89 @@ namespace SimpleDataSet
                 ReadOnly = true,
                 AllowDBNull = false,
                 Unique = true,
+                AutoIncrement = true,
+                AutoIncrementSeed = 1,
+                AutoIncrementStep = 1
             };
 
             var carMakeColumn = new DataColumn("Make", typeof (string));
             var carColorColumn = new DataColumn("Color", typeof(string)) { Caption = "Pet Name" };
-            var carPetNameColumn = new DataColumn("PetName", typeof(string)) ;       }
+            var carPetNameColumn = new DataColumn("PetName", typeof(string)) ;
+
+            var inventoryTable = new DataTable("Inventory");
+            inventoryTable.Columns.AddRange(new[] { carIDColumn, carMakeColumn, carColorColumn, carPetNameColumn });
+
+            DataRow carRow = inventoryTable.NewRow();
+            carRow["Make"] = "BMW";
+            carRow["Color"] = "Black";
+            carRow["PetName"] = "Hamlet";
+            inventoryTable.Rows.Add(carRow);
+            carRow = inventoryTable.NewRow();
+
+            carRow[1] = "Saab";
+            carRow[2] = "Red";
+            carRow[3] = "Sea Breeze";
+            inventoryTable.Rows.Add(carRow);
+
+            inventoryTable.PrimaryKey = new[] { inventoryTable.Columns[0] };
+
+            ds.Tables.Add(inventoryTable);
+        }
+
+        private static void ManipulateDataRowState()
+        {
+            var temp = new DataTable("Temp");
+            temp.Columns.Add(new DataColumn("TempColumn", typeof(int)));
+
+            var row = temp.NewRow();
+            WriteLine($"After calling NewRow(): {row.RowState}");
+
+            temp.Rows.Add(row);
+            WriteLine($"After calling Row.Add(): {row.RowState}");
+
+            row["TempColumn"] = 10;
+            WriteLine($"After first assignment: {row.RowState}");
+
+            temp.AcceptChanges();
+            WriteLine($"After calling AcceptChanges: {row.RowState}");
+
+            row["TempColumn"] = 11;
+            WriteLine($"After first assignment: {row.RowState}");
+
+            temp.Rows[0].Delete();
+            WriteLine($"After calling Delete: {row.RowState}");
+        }
+
+        static void PrintDataSet(DataSet ds)
+        {
+            WriteLine($"DataSet is named: {ds.DataSetName}");
+            foreach(DictionaryEntry de in ds.ExtendedProperties)
+            {
+                WriteLine($"Key = {de.Key}, Value = {de.Value}");
+            }
+            WriteLine();
+
+            foreach(DataTable dt in ds.Tables)
+            {
+                WriteLine($"=> {dt.TableName} Table:");
+
+                for (var curCol = 0; curCol < dt.Columns.Count; curCol++)
+                {
+                    Write($"{dt.Columns[curCol].ColumnName}\t");
+                }
+
+                WriteLine("\n---------------------------------------------");
+
+                for(var curRow = 0; curRow < dt.Rows.Count; curRow++)
+                {
+                    for(var curCol = 0; curCol < dt.Columns.Count; curCol++)
+                    {
+                        Write($"{dt.Rows[curRow][curCol]}\t");
+                    }
+                    WriteLine();
+                }
+            }
+        }
+        
     }
 }
