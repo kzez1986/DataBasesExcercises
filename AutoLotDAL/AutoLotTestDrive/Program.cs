054e429dd596401f7dd701da5aea67f3afb58290
unknown <jakub.zezula@hotmail.com>
2023-07-29 23:47:55 +0200

    Update AutoLot library

---
+++ AutoLotDAL/AutoLotTestDrive/Program.cs
@@ -0,0 +1,22 @@
+﻿using AutoLotDAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace AutoLotTestDrive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer(new DataInitializer());
+            WriteLine("***** Fun with ADO.NET EF Code First *****\n");
+            ReadLine();
+        }
+    }
+}
